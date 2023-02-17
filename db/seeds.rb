1a1a80fe45db5c9ff63a0be1165af041d4c2fc43
CaaaseyD <baimaxsolution@gmail.com>
2023-02-18 08:54:44 +1100

    redis added, seed added, config edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Doorkeeper::Application.create!(
+    name: "viteapp",
+    scopes: "read write",
+    redirect_uri: "http://localhost:3000/oauth/callback"
+)
