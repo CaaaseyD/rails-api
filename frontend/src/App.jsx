08ad0537ca9f0f7ba7a7af39880dd061f5d1f0b2
CaaaseyD <baimaxsolution@gmail.com>
2023-02-19 09:43:19 +1100

    hmr works(change config)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import {useEffect} from "react";
 import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
   const [user, setUser] = useState([])
+  // const client_id = import.meta.env.REACT_APP_CLIENT_ID;
+  // const client_secret = import.meta.env.REACT_APP_CLIENT_SECRET;
   const client_id = "ii1wZFkFkzfSuNMwDzrNhEshaX2Fo6Dc6QxCVAJEl8U";
   const client_secret = "SlBbmfaLnOfuQ3tesyHoi4BNyRC3HIa9jY5gLCOVqe4";
-
-  useEffect(()=>{
+  useEffect(() => {
       login();
     },[])
 
@@ -19,24 +18,23 @@ function App() {
       "email": "admin@caseydai.me",
       "password": "password",
       "client_id": client_id,
-      "client_secret": client_secret
-    }
+      "client_secret": client_secret,
+    };
     axios
-    .post("http://localhost:3000/v1/users/oauth/token", data)
-    .then(response => {
-      setUser(response.data.user)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+      .post("http://localhost:3000/v1/users/oauth/token", data)
+      .then(response => {
+        setUser(response.data.user);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
     <div className="App">
-      <div>
-        <h1>The API</h1>
-        <h2>Current User: {user}</h2>
-      </div>
+      <h1>The API</h1>
+      <h2>Current User: {user.email}</h2>
     </div>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,10 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.jsx"
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
 });
