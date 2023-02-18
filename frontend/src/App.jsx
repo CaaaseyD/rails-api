080a6f69cabff15073d4675f7140bb2e75c98da5
CaaaseyD <baimaxsolution@gmail.com>
2023-02-19 08:27:56 +1100

    hmr not working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import {useEffect} from "react";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0)
   const [user, setUser] = useState([])
   const client_id = "ii1wZFkFkzfSuNMwDzrNhEshaX2Fo6Dc6QxCVAJEl8U";
   const client_secret = "SlBbmfaLnOfuQ3tesyHoi4BNyRC3HIa9jY5gLCOVqe4";
+
+  useEffect(()=>{
+      login();
+    },[])
 
   const login = () => {
     const data = {
@@ -17,30 +22,21 @@ function App() {
       "client_secret": client_secret
     }
     axios
+    .post("http://localhost:3000/v1/users/oauth/token", data)
+    .then(response => {
+      setUser(response.data.user)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>The API</h1>
+        <h2>Current User: {user}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
